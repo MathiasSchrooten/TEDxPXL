883fe3eaa7ddff004cfce327599b46b7f5c2a2e2
Stephan Bisschop <stephanbisschop@hotmail.com>
2015-04-22 11:49:50 +0200

    Event
    Update, Delete, Insert, Select

--- application/models/Events_model.php
+++ application/models/Events_model.php
@@ -5,5 +5,19 @@ class Events_model extends CI_Model {
 		$query=$this->db->get('events');
 		return $query->result();
 	}
+	
+	public function update($id,$data){
+		$this->db->where('id', $id);
+		$this->db->update('events', $data);
+	}
+
+	public function delete($id){
+		$this->db->where('id', $id);
+		$this->db->delete('events');
+	}
+
+	public function insert($data){
+		$this->db->insert('events', $data);
+	}
 }
 ?>
