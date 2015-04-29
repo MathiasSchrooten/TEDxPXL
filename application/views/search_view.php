0469ba69ccf82967f51141ab4efbc481c74c13e1
JonasLod <jonaslodewyckx@gmail.com>
2015-04-29 12:07:50 +0200

    SimpleSearchAdd

---
+++ application/views/search_view.php
@@ -0,0 +1,13 @@
+<?php
+$this->load->view('header_view');
+?>
+<div id="port" class="portfolio portfolio-box">
+<div class="head text-center">
+  <h3><span> </span> Search</h3>
+  <p>Here you can search for stuff on TEDxPXL</p>
+  </br>
+</div>
+
+</div>
+</body>
+</html>
