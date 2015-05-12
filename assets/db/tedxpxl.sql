b3628b4cd2044556db0007d32117b16e594ef9ad
Stephan Bisschop <stephanbisschop@hotmail.com>
2015-05-12 12:06:14 +0200

    Database script

--- assets/db/tedxpxl.sql
+++ assets/db/tedxpxl.sql
@@ -1,11 +1,11 @@
-﻿-- phpMyAdmin SQL Dump
--- version 4.3.11
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Gegenereerd op: 07 mei 2015 om 16:12
--- Serverversie: 5.6.24
--- PHP-versie: 5.6.8
+-- Machine: 127.0.0.1
+-- Gegenereerd op: 12 mei 2015 om 12:05
+-- Serverversie: 5.6.21
+-- PHP-versie: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `tedxpxl`
+-- Databank: `tedxpxl`
 --
 
 -- --------------------------------------------------------
@@ -25,16 +25,11 @@ SET time_zone = "+00:00";
 --
 -- Tabelstructuur voor tabel `categories`
 --
-DROP TABLE categories;
-DROP TABLE comments;
-DROP TABLE events;
-DROP TABLE posts;
-DROP TABLE users;
 
 CREATE TABLE IF NOT EXISTS `categories` (
-  `CategorieId` int(11) NOT NULL,
+`CategorieId` int(11) NOT NULL,
   `Name` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `categories`
@@ -53,11 +48,11 @@ INSERT INTO `categories` (`CategorieId`, `Name`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `comments` (
-  `CommentId` int(11) NOT NULL,
+`CommentId` int(11) NOT NULL,
   `Text` varchar(250) NOT NULL,
   `PostId` int(11) NOT NULL,
   `UserId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -66,7 +61,7 @@ CREATE TABLE IF NOT EXISTS `comments` (
 --
 
 CREATE TABLE IF NOT EXISTS `events` (
-  `EventId` int(11) NOT NULL,
+`EventId` int(11) NOT NULL,
   `Title` varchar(50) NOT NULL,
   `Description` varchar(250) NOT NULL,
   `Date` date NOT NULL,
@@ -92,12 +87,12 @@ INSERT INTO `events` (`EventId`, `Title`, `Description`, `Date`, `Time`, `UserId
 --
 
 CREATE TABLE IF NOT EXISTS `posts` (
-  `PostId` int(11) NOT NULL,
+`PostId` int(11) NOT NULL,
   `Description` varchar(250) NOT NULL,
   `Title` varchar(50) NOT NULL,
   `UserId` int(11) NOT NULL,
   `CategorieId` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `posts`
@@ -117,7 +112,7 @@ INSERT INTO `posts` (`PostId`, `Description`, `Title`, `UserId`, `CategorieId`) 
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `UserId` int(11) NOT NULL,
+`UserId` int(11) NOT NULL,
   `Username` varchar(50) NOT NULL,
   `Password` varchar(50) NOT NULL,
   `Email` varchar(150) NOT NULL,
@@ -125,20 +120,21 @@ CREATE TABLE IF NOT EXISTS `users` (
   `Lastname` varchar(50) NOT NULL,
   `Role` int(11) NOT NULL,
   `Picture` varchar(50) NOT NULL,
-  `Signature` varchar(500) NOT NULL
+  `Signature` varchar(250) NOT NULL,
+  `About` varchar(250) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `users`
 --
 
-INSERT INTO `users` (`UserId`, `Username`, `Password`, `Email`, `Firstname`, `Lastname`, `Role`, `Picture`, `Signature`) VALUES
-(1, 'StephanB', '123', 'stephanbisschop@hotmail.com', 'Stephanes', 'Bisschop', 2, 'test.jpg', ' Loool'),
-(2, 'DylanGomes', '216692', 'dylangomes@live.be', 'Dylan', 'Gomes', 0, '', ''),
-(3, 'DarkSoully', '123', 'darkstormakadylan@gmail.com', 'Dylan', 'Gomes', 0, '', ''),
-(4, 'MathiasS', '123', 'mathiass@gmail.com', 'Mathias', 'Schrooten', 0, '', ''),
-(5, 'TomV', '123', 'tomv@gmail.com', 'Tom', 'Veuskens', 0, '', ''),
-(6, 'JonasL', '123', 'jonasl@gmail.com', 'Jonas', 'Lodewyckx', 0, '', '');
+INSERT INTO `users` (`UserId`, `Username`, `Password`, `Email`, `Firstname`, `Lastname`, `Role`, `Picture`, `Signature`, `About`) VALUES
+(1, 'StephanB', '123', 'stephanbisschop@hotmail.com', 'Stephan', 'Bisschop', 2, 'Profiel.jpg', ' Loool gfhgh', 'test'),
+(2, 'DylanGomes', '216692', 'dylangomes@live.be', 'Dylan', 'Gomes', 0, '', '', ''),
+(3, 'DarkSoully', '123', 'darkstormakadylan@gmail.com', 'Dylan', 'Gomes', 0, '', '', ''),
+(4, 'MathiasS', '123', 'mathiass@gmail.com', 'Mathias', 'Schrooten', 0, '', '', ''),
+(5, 'TomV', '123', 'tomv@gmail.com', 'Tom', 'Veuskens', 0, '', '', ''),
+(6, 'JonasL', '123', 'jonasl@gmail.com', 'Jonas', 'Lodewyckx', 0, '', '', '');
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -148,31 +144,31 @@ INSERT INTO `users` (`UserId`, `Username`, `Password`, `Email`, `Firstname`, `La
 -- Indexen voor tabel `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`CategorieId`);
+ ADD PRIMARY KEY (`CategorieId`);
 
 --
 -- Indexen voor tabel `comments`
 --
 ALTER TABLE `comments`
-  ADD PRIMARY KEY (`CommentId`);
+ ADD PRIMARY KEY (`CommentId`);
 
 --
 -- Indexen voor tabel `events`
 --
 ALTER TABLE `events`
-  ADD PRIMARY KEY (`EventId`);
+ ADD PRIMARY KEY (`EventId`);
 
 --
 -- Indexen voor tabel `posts`
 --
 ALTER TABLE `posts`
-  ADD PRIMARY KEY (`PostId`);
+ ADD PRIMARY KEY (`PostId`);
 
 --
 -- Indexen voor tabel `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`UserId`);
+ ADD PRIMARY KEY (`UserId`);
 
 --
 -- AUTO_INCREMENT voor geëxporteerde tabellen
@@ -182,27 +178,27 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT voor een tabel `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `CategorieId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `CategorieId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT voor een tabel `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `CommentId` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `CommentId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT voor een tabel `events`
 --
 ALTER TABLE `events`
-  MODIFY `EventId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `EventId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT voor een tabel `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `PostId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `PostId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT voor een tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `UserId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+MODIFY `UserId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
